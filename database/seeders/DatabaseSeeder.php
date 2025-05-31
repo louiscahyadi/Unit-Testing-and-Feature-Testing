e5ba0cc34dd1598a5b213bb57db896fcbedc5701
louiscahyadi <loouis1911@gmail.com>
2025-05-31 15:45:53 +0800

    Changes article seeding from user to article factory
    Updates the database seeder to create test articles instead of test users, maintaining the same test data structure with name and email fields.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Article;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,7 +16,7 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
+        Article::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
